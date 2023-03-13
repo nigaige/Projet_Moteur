adcdb464db6a12f99e2e33f62693b06d9e8e0b97
kchapuis <98804154+Kchapuis@users.noreply.github.com>
2023-03-13 14:57:40 +0100

    V1 Transform

--- Project_Moteur_Lib/Project_Moteur_Lib/Transform.h
+++ Project_Moteur_Lib/Project_Moteur_Lib/Transform.h
@@ -14,17 +14,22 @@ class Transform
 private :
 	D3DXVECTOR3 vPosition ;
 	D3DXVECTOR3 vScale;
-	D3DXMATRIX mRendu;
 
+	D3DXMATRIX mRot;
+	D3DXMATRIX mScale;
+	D3DXMATRIX mPos ;
+	D3DXMATRIX mRendu;
 
 	float Roll, Yaw, Pitch;
 
-	D3DXQUATERNION quat;
+	D3DXQUATERNION quatRot;
 	D3DXVECTOR3 vDir = D3DXVECTOR3(1.0f,0.0f,0.0f);
 	D3DXVECTOR3 vRight = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
 	D3DXVECTOR3 vUp = D3DXVECTOR3(0.0f, 0.0f, 1.0f);
 
 public:
+	Transform();
+	~Transform();
 	void SetvPos(D3DXVECTOR3 NewPos);
 	void SetvScale(D3DXVECTOR3 NewScale);
 
@@ -36,10 +41,10 @@ public:
 
 
 
-	void Rotate(float NewYaw, float NewPitch, float NewRoll );
+	void SetRotation(float NewYaw, float NewPitch, float NewRoll );
 	float DegToRad(float Angle);
 
-
+	
 
 };
 
