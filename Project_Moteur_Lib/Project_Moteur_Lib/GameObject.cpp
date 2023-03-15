a6a5e0959ef572eb58019a2c4962b57d827ff7de
nigaige <nicolas.gaige@free.fr>
2023-03-15 13:31:23 +0100

    fix uncommited file

--- Project_Moteur_Lib/Project_Moteur_Lib/GameObject.cpp
+++ Project_Moteur_Lib/Project_Moteur_Lib/GameObject.cpp
@@ -2,13 +2,13 @@
 
 GameObject::GameObject()
 {
-	toDisplay = false;
+	toDisplay_ = false;
 }
 
-GameObject::GameObject(Transform T)
+GameObject::GameObject(Transform* T)
 {
-	toDisplay = false;
-	transform_ = &T;
+	toDisplay_ = false;
+	transform_ = T;
 }
 
 void GameObject::addComponent(AbstractComponent* comp)
@@ -37,4 +37,22 @@ void GameObject::rmComponent(int index)
 {
 	delete componentList[index];
 	componentList[index] = new AbstractComponent;
+}
+
+template<typename T>
+inline T* GameObject::findComponent()
+{
+	/*
+	Call with:
+	gameobject.findcomponent<type>()
+	*/
+
+	for (Component comp : componentList)
+	{
+		if (typeid(*comp) == typeid(T)) {
+			return static_cast<T*>(comp);
+		}
+	}
+
+	return nullptr;
 }
